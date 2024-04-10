6e71e92e0be4d48b1a0d0193225a39a16f2cb72b
Jason Tolen <Jtolen543@gmail.com>
2024-04-10 12:10:49 -0400

    Added the decode method

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # this is my first github addition
 
-print('hello world')
+def decode(password):
+    list1 = [int(i) for i in password]
+    list1 = [str(i - 3) for i in list1]
+    password = "".join(list1)
+    return password
+
+
